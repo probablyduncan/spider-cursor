fc6279d530789f49ea98daef8498b392b9eb0b81
probablyduncan <duncanpetrie1@gmail.com>
2025-08-01 20:42:43 +0100

    sound!

--- src/spider.ts
+++ src/spider.ts
@@ -163,6 +163,17 @@ function drawLegs(centerPos: Vec2, centerRotation: Vec2) {
             // reset foot if not already set, if too far from ideal, or randomly if idle
             if (!currentPos || currentPos.subtract(idealPos).magnitude() > legStretch || (noMoveTimeoutId && Math.random() > 0.99)) {
                 feetPositions[legIndex][sideIndex] = currentPos = idealPos;
+
+                const now = new Date().valueOf();
+                const lerpedFromCenter = idealPos.subtract(Vec2.From(window.innerWidth / 2, window.innerHeight / 2))
+                    .abs()
+                    .unlerp(Vec2.Zero, Vec2.From(window.innerWidth / 2, window.innerHeight / 2))
+                    .magnitude();
+
+                if (lastPlay < now - 50) {
+                    lastPlay = now;
+                    playNoteFromLerp(Math.max(0, Math.min(1, 1 - lerpedFromCenter)));
+                }
             }
 
             ctx.beginPath();
@@ -248,3 +259,60 @@ scaleCanvasToWindow();
 setIdleTimeout();
 requestAnimationFrame(animate);
 
+
+//#region audio
+// https://css-tricks.com/introduction-web-audio-api/
+
+let audioContext: AudioContext | undefined;
+const button = document.querySelector("button[data-sound-toggle]") as HTMLButtonElement;
+const toggles = document.querySelectorAll("[data-toggle-with-sound]");
+button?.addEventListener("click", () => {
+    const altContent = button.dataset.soundToggle ?? "";
+    button.dataset.soundToggle = button.innerHTML;
+    button.innerHTML = altContent;
+
+    toggles.forEach(_e => {
+        (_e as HTMLElement).toggleAttribute("hidden");
+    })
+
+    audioContext = audioContext ? undefined : new AudioContext();
+});
+
+let lastPlay = 0;
+const frequencies = [
+    195.9977, 207.6523, 220.0000, 233.0819, 246.9417, 261.6256, 277.1826, 293.6648, 311.1270, 329.6276, 349.2282, 369.9944, 391.9954, 415.3047, 440.0000, 466.1638, 493.8833, 523.2511, 554.3653, 587.3295, 622.2540, 659.2551, 698.4565, 739.9888, 783.9909, 830.6094, 880.0000, 932.3275, 987.7666, 1046.502, 1108.731, 1174.659, 1244.508, 1318.510, 1396.913, 1479.978, 1567.982, 1661.219, 1760.000,
+]
+
+function playNoteFromLerp(lerp: number) {
+    playNote(frequencies[Math.floor(frequencies.length * lerp)]);
+}
+
+function playNote(frequency: number) {
+
+    if (!audioContext) {
+        return;
+    }
+
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+    
+    const time = audioContext.currentTime;
+    
+    oscillator.type = 'triangle';
+    oscillator.frequency.value = frequency;
+
+    // volume
+    gainNode.gain.setValueAtTime(0, time);
+    gainNode.gain.exponentialRampToValueAtTime(0.02, time + 0.01);
+
+    oscillator.start(time);
+
+    // stop
+    gainNode.gain.exponentialRampToValueAtTime(0.001, time + 1);
+    oscillator.stop(time + 1);
+}
+
+//#endregion
